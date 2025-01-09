068cc2705aa92d14e81c51561eb30bac3fd66ced
elangabdulazis <adamracing54@gmail.com>
2025-01-09 14:52:43 +0700

    Penambhan Fungsi bagi

--- src/main/java/parttwo/Fungsi.java
+++ src/main/java/parttwo/Fungsi.java
@@ -7,11 +7,16 @@ public class Fungsi {
     public static int kuadrat (int y){
         return y*y;
     }
+    public  static  int bagi (int a, int b){
+        return a/b;
+    }
     public static void main (String[]args){
         int tambah = tambah(8,10);
         int kuadrat = kuadrat(6);
+        int bagi =  bagi(100,5);
         System.out.println("Hasil Dari Tambah = " + tambah);
         System.out.println("Hasil dari Kuadrat = " + kuadrat);
+        System.out.println("Hasil bagi = "+bagi);
     }
 }
 
